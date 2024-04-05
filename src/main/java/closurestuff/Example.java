38999ffb1836011901bb775f4ec47feb789fa39d
simon <simon@dancingcloudservices.com>
2024-04-05 09:19:09 -0700

    4/5 Function combinator example

---
+++ src/main/java/closurestuff/Example.java
@@ -0,0 +1,61 @@
+package closurestuff;
+
+import java.io.Serializable;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+@FunctionalInterface
+interface X {
+  void doStuff();
+//  void doOtherMethod();
+}
+
+interface Y {
+  void doOtherMethod();
+}
+
+interface Z extends X, Y {
+
+}
+
+
+public class Example {
+  public static <T> Predicate<T> getBothPredicate(Predicate<T> ps1, Predicate<T> ps2) {
+    return s -> ps1.test(s) && ps2.test(s);
+  }
+
+  public static Predicate<String> getShortStringPredicate(final int threshold) {
+    return s -> s.length() < threshold;
+  }
+  public static Predicate<String> getLongStringPredicate(final int threshold) {
+//    threshold++;
+    return s -> s.length() > threshold;
+  }
+  public static void main(String[] args) {
+//    Comparable & Constable & Serializable o = false ? "Hello" : 99;
+    var o = false ? "Hello" : 99;
+//    "".cons
+//    "".com
+//    Integer.valueOf(99).com..
+
+//    X x = () -> {};
+
+    Predicate<String> lsp = getLongStringPredicate(4);
+    Stream.of("Fred", "Jim", "Shiela", "Alice", "Bob", "Xi", "I")
+        .filter(lsp)
+        .forEach(System.out::println);
+    System.out.println("-------------------");
+    lsp = getLongStringPredicate(3);
+    Stream.of("Fred", "Jim", "Shiela", "Alice", "Bob")
+        .filter(lsp)
+        .forEach(System.out::println);
+
+    System.out.println("-------------------");
+    Predicate<String> longer = getLongStringPredicate(3);
+    Predicate<String> shorter = getShortStringPredicate(6);
+    Predicate<String> both = getBothPredicate(longer, shorter);
+    Stream.of("Fred", "Jim", "Shiela", "Alice", "Bob")
+        .filter(both)
+        .forEach(System.out::println);
+  }
+}
